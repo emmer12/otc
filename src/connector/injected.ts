1ebaa807799d1eed1e9aac9a0cc2d9587280e889
emmer12 <laffbaze@gmail.com>
2023-08-26 08:19:37 +0100

    Aff btn loader

--- src/connector/injected.ts
+++ src/connector/injected.ts
@@ -2,7 +2,7 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { NetworkConnector } from "@web3-react/network-connector";
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 97, 56, 137, 43113, 43114, 80001, 42161],
+  supportedChainIds: [1, 3, 4, 5, 42, 97, 56, 137, 43113, 43114, 80001, 42161, 8453, 5000],
 });
 
 export const network = new NetworkConnector({
