d725beb9b85724627dc2c3baadd82238d947250a
emmer12 <laffbaze@gmail.com>
2023-09-25 11:15:43 +0100

    Otc fix

--- dist/assets/p2p.6b12d616.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as r,T as t}from"./index.2cdbdff1.js";const s=()=>e(r,{align:"center",justify:"center",children:e(t,{uppercase:!0,size:"big",children:"Coming Soon"})});export{s as default};
