cdc10920c572ed60d164783e0178eb28f7ab11f9
emmer12 <laffbaze@gmail.com>
2023-09-25 10:48:27 +0100

    Otc fic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,11 @@ import {
   HowTo,
   KybarSwap,
   Widget,
+  CreateList,
+  List,
+  Home,
+  Trades,
+  P2p,
 } from "../views";
 import MainLayout from "../views/layouts/main";
 import Dashboard from "../views/layouts/dashboard";
@@ -26,25 +31,25 @@ const AnimatedRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomeLazy />} />
-        <Route path="/trades" element={<TradesLazy />} />
-        <Route path="/list/create" element={<CreateListLazy />} />
-        <Route path="/list" element={<ListLazy />} />
+        <Route index element={<Home />} />
+        <Route path="/trades" element={<Trades />} />
+        <Route path="/list/create" element={<CreateList />} />
+        <Route path="/list" element={<List />} />
         <Route path="/chart" element={<Chart />} />
-        <Route path="/p2p" element={<P2pLazy />} />
+        <Route path="/p2p" element={<P2p />} />
         <Route path="/how-to" element={<HowTo />} />
         <Route path="/swap" element={<KybarSwap />} />
         <Route path="/widget" element={<Widget />} />
         <Route path="/test-tokens" element={<Claim />} />
         <Route path="doc" element={<Escrow />} />
-        <Route path="/trades/:id" element={<TradesLazy />} />
+        <Route path="/trades/:id" element={<Trades />} />
         <Route path="/dashboard" element={<Dashboard />}>
           <Route index element={<MyListings />} />
           <Route path="/dashboard/my-listings" element={<MyListings />} />
           <Route path="/dashboard/profile" element={<Profile />} />
           <Route path="/dashboard/history" element={<History />} />
         </Route>
-        <Route path="/:chain" element={<HomeLazy />} />
+        <Route path="/:chain" element={<Home />} />
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
